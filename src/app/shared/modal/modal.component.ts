842cfe103c2eb918af1c44d8d1683aba643cbbbe
rebeca27 <38529211+rebeca27@users.noreply.github.com>
2022-12-22 11:34:45 +0200

    project updates

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ModalService} from "../../services/modal.service";
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit{
+
+  @Input() modalID = ''
+  constructor(public modal: ModalService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID)
+
+  }
+}
